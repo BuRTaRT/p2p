2c88317139361e88ee86c265e48fc86e1f1095a2
Artem <bburtart@gmail.com>
2024-09-16 16:26:18 +0300

    3

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -6,7 +6,7 @@ import {useState} from "react";
 
 const PopUp = ({onIsInvisible}) => {
     const [phone, setPhone] = useState('');
-    const [submitted,setSubmitted]=[useState(false)]
+    const [submitted, setSubmitted] = useState(false)
     const {
         register,
         trigger,
@@ -28,68 +28,78 @@ const PopUp = ({onIsInvisible}) => {
         setPhone('')
         reset();
         console.log(data)
-
+        setSubmitted(true)
     }
 
 
+    let content = <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className={s.input}>
+            <input
+                {...register('name', {
+                    required: 'Имя обязательно',
+                    pattern: {
+                        value: /^[a-zA-Zа-яА-Я]{3,20}$/i,
+                        message: 'Введите корректное имя',
+                    }
+                })} type="text" placeholder={'Имя'}/>
+            {errors.name && <p className={s.error}>{errors.name.message}</p>}
+        </div>
+        <div className={s.input}>
+            <PhoneInput
+                country={'ru'}
+                value={phone}
+                onChange={(phone) => {
+                    setValue('phone', phone)
+                    trigger('phone');
+                    setPhone(phone);  // Обновляем состояние phone
+                }}
+                inputClass={s['phone-input']}
+                inputProps={{
+                    name: 'phone',
+                    required: true,
+                    autoFocus: false
+                }}
+            />
+            {errors.phone && <p className={s.error}>{errors.phone.message}</p>}
+        </div>
+        <input
+            type="hidden"
+            {...register('phone', {
+                required: 'Телефон обязателен',
+                validate: (value) => value.length >= 10 || 'Номер телефона должен быть не менее 10 цифр',
+            })}
+        />
+        <div className={s.input}>
+            <input type="text" placeholder={'Email'}
+                   {...register('email', {
+                       required: 'Email обязателен',
+                       pattern: {
+                           value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                           message: 'Введите корректный email',
+                       },
+                   })}
+
+            />
+            {errors.email && <p className={s.error}>{errors.email.message}</p>}
+        </div>
+        <button className={s.submit} type={"submit"}>submit</button>
+    </form>
+
+    if (submitted) {
+        content = <div className={s.submitted}>
+            Спасибо! Данные успешно отправлены.
+        </div>
+    }
+
     return (
         <div onMouseDown={onIsInvisible} className={s.wrap}>
+            <img src="/x-mark-512.png" alt=""/>
             <div onMouseDown={(e) => e.stopPropagation()} className={s.popup}>
                 <h2>Оставь заявку</h2>
                 <p>Наш специалист свяжется и присоединит к Вас закрытой бесплатной группе</p>
 
-                <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-                    <div className={s.input}>
-                        <input
-                            {...register('name', {
-                            required: 'Имя обязательно',
-                            pattern: {
-                                value: /^[a-zA-Zа-яА-Я]{3,20}$/i,
-                                message: 'Введите корректное имя',
-                            }
-                        })} type="text" placeholder={'Имя'}/>
-                        {errors.name && <p className={s.error}>{errors.name.message}</p>}
-                    </div>
-                    <div className={s.input}>
-                        <PhoneInput
-                            country={'ru'}
-                            value={phone}
-                            onChange={(phone) => {
-                                setValue('phone', phone)
-                                trigger('phone');
-                                setPhone(phone);  // Обновляем состояние phone
-                            }}
-                            inputClass={s['phone-input']}
-                            inputProps={{
-                                name: 'phone',
-                                required: true,
-                                autoFocus: false
-                            }}
-                        />
-                        {errors.phone && <p className={s.error}>{errors.phone.message}</p>}
-                    </div>
-                    <input
-                        type="hidden"
-                        {...register('phone', {
-                            required: 'Телефон обязателен',
-                            validate: (value) => value.length >= 10 || 'Номер телефона должен быть не менее 10 цифр',
-                        })}
-                    />
-                    <div className={s.input}>
-                        <input type="text" placeholder={'Email'}
-                               {...register('email', {
-                                   required: 'Email обязателен',
-                                   pattern: {
-                                       value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                                       message: 'Введите корректный email',
-                                   },
-                               })}
+                {content}
 
-                        />
-                        {errors.email && <p className={s.error}>{errors.email.message}</p>}
-                    </div>
-                    <button className={s.submit} type={"submit"}>submit</button>
-                </form>
             </div>
         </div>
     );
